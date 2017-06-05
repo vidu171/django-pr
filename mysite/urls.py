a8021800165b377b6c48511c64a56473e9e95a5a
vidu171 <v.vidu171@gmail.com>
2017-06-05 12:14:26 +0530

    rest_framework view added

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,11 +19,14 @@ from django.conf.urls import include
 from rest_framework import routers
 from books import views
 
+router = routers.DefaultRouter()
+router.register(r'bookadd', views.BookViewSet)
+
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-   
+    url(r'^', include(router.urls)),
     url(r'', include('books.urls')),
 ]
